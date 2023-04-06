c1fdf26422a2980424557ff78c8392a230627d00
Valentina Perea Marquez <78619463+202013095@users.noreply.github.com>
2023-04-06 02:03:48 -0500

    Update Hotel.java

--- src/Procesamiento/Hotel.java
+++ src/Procesamiento/Hotel.java
@@ -154,20 +154,71 @@ import modelo.Usuario;
 	}
 	
 	/*FUNCIONES ADMINISTRADOR*/
+	
+/*cambiarPrecioBaseHabitacion(info)*/
+	
+public String cambiarPrecioBaseHabitacion(String info, String precio) {
+	
+	camas.get(info).setPrecio(Integer.parseInt(precio));
+	String ejecucion="El cambio de precio de la cama"+info+"se realizo de manera exitosa.";
+	return ejecucion;
+}
+	
+/*Cambiar Cama de una adiciones*/
+	
+	public String cambiarAdiciones(String info, String info2) {
+		
+		String ejecucion="La habitacion que usted busca no existe en el hotel";
+		for (Map.Entry<Integer,Habitacion> habitacion : habitaciones.entrySet()) {
+			if (habitacion.getKey().equals(Integer.parseInt(info))) {
+				habitacion.getValue().cambiarAdiciones(adicion,info2);
+				ejecucion="Se cambiaron las camas de manera exitosa";	
+				break;
+			}
+			}
+		return ejecucion;}
+	
+	/*Cambiar Cama de una habitacion*/
+	
+	public String cambiarCamas(String info, String info2) {
+		
+		String ejecucion="La habitacion que usted busca no existe en el hotel";
+		for (Map.Entry<Integer,Habitacion> habitacion : habitaciones.entrySet()) {
+			if (habitacion.getKey().equals(Integer.parseInt(info))) {
+				habitacion.getValue().cambiarCamas(camas,info2);
+				ejecucion="Se cambiaron las camas de manera exitosa";	
+				break;
+			}
+			}
+		return ejecucion;}
+	
 	/*Agregar Habitacion*/
 	
 	public String crearHabitacion(String info) throws FileNotFoundException, IOException {
 		
-		System.out.println("pato");
 		habitaciones = Loader.crearyaniadirHabitacion(habitaciones, info);
 		
 		System.out.println("Habitacion creada con exito");
 		return "Habitacion creada con exito";
 		
 	}
+
+	public String cambiarPrecioHabitaciones(String precio) {
+		String ejecucion="La habitacion que usted busca no existe en el hotel";
+		for (Map.Entry<Integer,Habitacion> habitacion : habitaciones.entrySet()) {
+			if (Integer.parseInt(precio)>0) {
+				habitacion.getValue().aumentotarifa(Integer.parseInt(ejecucion));
+			}
+			else {
+				habitacion.getValue().descuentotarifa(Integer.parseInt(ejecucion));
+			}
+	}
+		return ejecucion;}
 		
 		
 	
+
+	}
 
 	}
 	
