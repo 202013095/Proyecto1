aca284c5ea8d033318556ac5f57ab4b82c1d4cfe
monicagonz <[m.gonzalezv2@uniandes.edu.co]>
2023-04-06 00:15:48 -0500

    zzz

---
+++ src/modelo/Guia_Turistica.java
@@ -0,0 +1,17 @@
+package modelo;
+
+public class Guia_Turistica extends Servicio{
+
+	public Guia_Turistica(float precio, boolean disponibilidad) {
+		super(precio, disponibilidad);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void nuevaGuia() {
+		
+		
+		
+	}
+
+}
+ 
---
+++ src/modelo/Restaurante.java
@@ -0,0 +1,22 @@
+package modelo;
+
+
+public class Restaurante extends Servicio{
+	
+	
+
+	public Restaurante(float precio, boolean disponibilidad) {
+		super(precio, disponibilidad);
+		// TODO Auto-generated constructor stub
+			
+		
+	}
+
+	public void nuevoRestaurante() {
+		
+	}
+	
+
+
+}
+ 
